6bd2a5b3615e36f5c30454125fe3dc8f27362343
Puran Singh <purandixit@gmail.com>
2023-11-15 16:41:34 +0530

    createFeed handler added

--- handler_feed.go
+++ handler_feed.go
@@ -10,35 +10,32 @@ import (
 	"github.com/purandixit07/blogAggregator_go/internal/database"
 )
 
-func (apiCfg *apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Request) {
+func (apiCfg *apiConfig) handlerCreateFeed(w http.ResponseWriter, r *http.Request, user database.User) {
 	type parameters struct {
 		Name string `json:"name"`
+		URL  string `json:"url"`
 	}
 
 	decoder := json.NewDecoder(r.Body)
 	params := parameters{}
 	err := decoder.Decode(&params)
-
 	if err != nil {
 		respondWithError(w, 400, fmt.Sprintf("Error parsing JSON: %v", err))
 		return
 	}
 
-	user, err := apiCfg.DB.CreateUser(r.Context(), database.CreateUserParams{
+	feed, err := apiCfg.DB.CreateFeed(r.Context(), database.CreateFeedParams{
 		ID:        uuid.New(),
 		CreatedAt: time.Now().UTC(),
 		UpdatedAt: time.Now().UTC(),
 		Name:      params.Name,
+		Url:       params.URL,
+		UserID:    user.ID,
 	})
 
 	if err != nil {
-		respondWithError(w, 400, fmt.Sprintf("Couldn't creat user: %v", err))
-		return
+		respondWithError(w, 400, fmt.Sprintf("Couldn't create feed: %v", err))
 	}
 
-	respondWithJson(w, 201, user)
-}
-
-func (apiCfg *apiConfig) handlerGetUser(w http.ResponseWriter, r *http.Request, user database.User) {
-	respondWithJson(w, 200, user)
+	respondWithJson(w, 201, feed)
 }
---
+++ middleware_auth.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/purandixit07/blogAggregator_go/internal/auth"
+	"github.com/purandixit07/blogAggregator_go/internal/database"
+)
+
+type authedHandler func(http.ResponseWriter, *http.Request, database.User)
+
+func (apiCfg *apiConfig) middlewareAuth(handler authedHandler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		apiKey, err := auth.GetApiKey(r.Header)
+		if err != nil {
+			respondWithError(w, 403, fmt.Sprintf("Auth error: %v", err))
+		}
+
+		user, err := apiCfg.DB.GetUserByAPIKey(r.Context(), apiKey)
+		if err != nil {
+			respondWithError(w, 400, fmt.Sprintf("Couldn't get user: %v", err))
+		}
+		handler(w, r, user)
+	}
+}
