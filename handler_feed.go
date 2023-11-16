436e72e49f1069370e0e18c8187376f4aa7859a8
Puran Singh <purandixit@gmail.com>
2023-11-16 15:15:22 +0530

    feeds handler created

--- handler_feed.go
+++ handler_feed.go
@@ -39,3 +39,12 @@ func (apiCfg *apiConfig) handlerCreateFeed(w http.ResponseWriter, r *http.Reques
 
 	respondWithJson(w, 201, feed)
 }
+
+func (apiCfg *apiConfig) handlerGetFeed(w http.ResponseWriter, r *http.Request) {
+	feeds, err := apiCfg.DB.GetFeeds(r.Context())
+	if err != nil {
+		respondWithError(w, 400, fmt.Sprintf("Couldn't get feeds: %v", err))
+		return
+	}
+	respondWithJson(w, 200, feeds)
+}
