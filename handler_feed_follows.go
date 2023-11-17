9ac6382ba8d6521d498cce8f10260d2938361158
Puran Singh <purandixit@gmail.com>
2023-11-17 16:04:53 +0530

    delete feed route added

--- handler_feed_follows.go
+++ handler_feed_follows.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/go-chi/chi"
 	"github.com/google/uuid"
 	"github.com/purandixit07/blogAggregator_go/internal/database"
 )
@@ -49,4 +50,24 @@ func (apiCfg *apiConfig) handlerGetFeedFollow(w http.ResponseWriter, r *http.Req
 	}
 
 	respondWithJson(w, 201, feedFollows)
+}
+
+func (apiCfg *apiConfig) handlerDeleteFeedFollow(w http.ResponseWriter, r *http.Request, user database.User) {
+	feedFollowIDStr := chi.URLParam(r, "feedFollowID")
+	feedFollowID, err := uuid.Parse(feedFollowIDStr)
+	if err != nil {
+		respondWithError(w, 400, fmt.Sprintf("Can't parse feed follow id: %v", err))
+		return
+	}
+	err = apiCfg.DB.DeleteFeedFollows(r.Context(), database.DeleteFeedFollowsParams{
+		ID:     feedFollowID,
+		UserID: user.ID,
+	})
+
+	if err != nil {
+		respondWithError(w, 400, fmt.Sprintf("Can't delete feed follow: %v", err))
+		return
+	}
+
+	respondWithJson(w, 200, struct{}{})
 }
